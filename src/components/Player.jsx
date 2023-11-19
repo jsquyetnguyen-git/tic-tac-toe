646a24fc3fbb6531b2e0ea8271c112acc86da962
Js Quyet Nguyen <jsquyetnguyen@gmail.com>
2023-11-19 15:21:33 +0700

    test

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 export default function Player({name, symbol}) {
+
+    const [isEditting, setIsEditting] = useState(false);
+
+    const handleEditClick = () => {
+        setIsEditting(!isEditting);
+        console.info(isEditting);
+    }
+
     return (
         <li>
             <span className="player-info">
                 <span className="player-name">{name}</span>
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick = {handleEditClick}>Edit</button>
         </li>
     )
 }
