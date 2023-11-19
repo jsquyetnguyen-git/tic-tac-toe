946d9f283ac8281c0db911932907624f311cc940
Js Quyet Nguyen <jsquyetnguyen@gmail.com>
2023-11-19 16:20:19 +0700

    commit for 74 cConcept Repetition: Working with State

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,17 +5,23 @@ function Player({name, symbol}) {
     const [isEditting, setIsEditting] = useState(false);
 
     const handleEditClick = () => {
-        setIsEditting(!isEditting);
+        setIsEditting(true);
         console.info(isEditting);
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+
+    if (isEditting) {
+        playerName = <input type="text" required/>
     }
 
     return (
         <li>
-            <span className="player-info">
-                <span className="player-name">{name}</span>
+            <span className="player">
+                {playerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick = {handleEditClick}>Edit</button>
+            <button onClick = {handleEditClick}>{!isEditting? "Edit":"Ok"}</button>
         </li>
     )
 }
