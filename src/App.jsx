9ca632410ccd5779ee2635c25582fa497059255e
Js Quyet Nguyen <jsquyetnguyen@gmail.com>
2023-11-23 22:49:11 +0700

    add log

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import { useState } from "react";
+import Log from "./components/Log";
 
 function App() {
   const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
 
-  const handleSelect = (e) => {
-    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X'?'O':'X');
+
+  const handleSelect = (rowIndex, colIndex) => {
+    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns((prevTurns) => {
+      let currentPlayer = 'X';
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+      const updateTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns];
+      return updateTurns;
+    });
   }
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players" className = "highlight-player">
+        <ol id="players" className="highlight-player">
           <Player isActive={activePlayer === 'X'} name="Player 1" symbol="X" />
           <Player isActive={activePlayer === 'O'} name="Player 2" symbol="O" />
         </ol>
-        <GameBoard activePlayer={activePlayer} onSelectSquare={handleSelect} />
+        <GameBoard turns = {gameTurns} activePlayer={activePlayer} onSelectSquare={handleSelect} />
       </div>
+      <Log turns={gameTurns}/>
     </main>
   );
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 const initGame = [
   [null, null, null, null, null, null],
   [null, null, null, null, null, null],
@@ -9,17 +7,26 @@ const initGame = [
   [null, null, null, null, null, null],
 ];
 
-function GameBorad({onSelectSquare, activePlayer}) {
-  const [gameBoard, setGameBoard] = useState(initGame);
+function GameBorad({ onSelectSquare, turns }) {
+  let gameBoard = initGame;
 
-   const handleClick = (rowIndex, colIndex) => {
-    setGameBoard((prevGameBorad) => {
-        const updateGameBoard = [... prevGameBorad.map(innderArray => [...innderArray]) ];
-        updateGameBoard[rowIndex][colIndex] = activePlayer;
-        return updateGameBoard;
-    });
-    onSelectSquare();
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square
+
+    gameBoard[row][col] = player;
   }
+
+  // const [gameBoard, setGameBoard] = useState(initGame);
+
+  //  const handleClick = (rowIndex, colIndex) => {
+  //   setGameBoard((prevGameBorad) => {
+  //       const updateGameBoard = [... prevGameBorad.map(innderArray => [...innderArray]) ];
+  //       updateGameBoard[rowIndex][colIndex] = activePlayer;
+  //       return updateGameBoard;
+  //   });
+  //   onSelectSquare();
+  // }
 
   return (
     <ol id="game-board">
@@ -27,7 +34,7 @@ function GameBorad({onSelectSquare, activePlayer}) {
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
-              <button onClick = {() => handleClick(rowIndex, colIndex)} key={colIndex}>{playerSymbol}</button>
+              <button onClick={() => onSelectSquare(rowIndex, colIndex)} key={colIndex}>{playerSymbol}</button>
             ))}
           </ol>
         </li>
